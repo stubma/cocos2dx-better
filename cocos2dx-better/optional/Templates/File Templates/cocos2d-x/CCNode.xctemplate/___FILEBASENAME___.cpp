644dcc9217656eb906dcf1956af9240c55d39255
luma <stubma@gmail.com>
2014-03-31 21:34:05 +0800

    add template for ccnode and ccnodergba

---
+++ cocos2dx-better/optional/Templates/File Templates/cocos2d-x/CCNode.xctemplate/___FILEBASENAME___.cpp
@@ -0,0 +1,30 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+___FILEBASENAME___::___FILEBASENAME___() {
+	
+}
+
+___FILEBASENAME___::~___FILEBASENAME___() {
+	
+}
+
+___FILEBASENAME___* ___FILEBASENAME___::create() {
+	___FILEBASENAME___* l = new ___FILEBASENAME___();
+	l->init();
+	return (___FILEBASENAME___*)l->autorelease();
+}
+
+bool ___FILEBASENAME___::init() {
+	if(!CCNode::init())
+		return false;
+	
+	return true;
+}

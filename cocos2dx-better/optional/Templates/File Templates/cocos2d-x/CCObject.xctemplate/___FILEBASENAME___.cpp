a418a13d528e96046bf827a9dfa8d9c7669d1187
luma <stubma@gmail.com>
2014-03-31 21:29:06 +0800

    add CCObject template

---
+++ cocos2dx-better/optional/Templates/File Templates/cocos2d-x/CCObject.xctemplate/___FILEBASENAME___.cpp
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include "___FILEBASENAME___.h"
+
+___FILEBASENAME___::___FILEBASENAME___() {
+	
+}
+
+___FILEBASENAME___::~___FILEBASENAME___() {
+	
+}
+
+___FILEBASENAME___* ___FILEBASENAME___::create() {
+	___FILEBASENAME___* l = new ___FILEBASENAME___();
+	l->init();
+	return (___FILEBASENAME___*)l->autorelease();
+}
+
+bool ___FILEBASENAME___::init() {
+	return true;
+}
---
+++ cocos2dx-better/optional/Templates/File Templates/cocos2d-x/CCObject.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,23 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+class ___FILEBASENAMEASIDENTIFIER___ : public CCObject {
+protected:
+	___FILEBASENAMEASIDENTIFIER___();
+	
+public:
+	virtual ~___FILEBASENAMEASIDENTIFIER___();
+	static ___FILEBASENAMEASIDENTIFIER___* create();
+	
+	virtual bool init();
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
